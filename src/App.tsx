4c1d1078b9a1e5ea3bedefdeb427666b471f30c9
Shay <sshay77@hotmail.com>
2023-05-02 11:20:00 +0300

    Added graphql and storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
@@ -31,16 +32,33 @@ const App = ({ signOut }: props) => {
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes }) as any;
     const notesFromAPI = apiData.data.listNotes.items;
+
+    await Promise.all(
+      notesFromAPI.map(async (note: any) => {
+        if (note.image) {
+          const url = await Storage.get(note.name);
+          note.image = url;
+        }
+        return note;
+      })
+    );
+
     setNotes(notesFromAPI);
   }
 
   async function createNote(event: any) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image") as any;
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name
     };
+    if (!!data.image) {
+      await Storage.put(data.name as any, image);
+    }
+
     await API.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -49,9 +67,10 @@ const App = ({ signOut }: props) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id } :any) {
-    const newNotes = notes.filter((note:any) => note.id !== id);
+  async function deleteNote({ id, name }: any) {
+    const newNotes = notes.filter((note: any) => note.id !== id);
     setNotes(newNotes);
+    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -79,6 +98,12 @@ const App = ({ signOut }: props) => {
             variation="quiet"
             required
           />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
@@ -86,7 +111,7 @@ const App = ({ signOut }: props) => {
       </View>
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
-        {notes.map((note:any) => (
+        {notes.map((note: any) => (
           <Flex
             key={note.id || note.name}
             direction="row"
@@ -97,6 +122,13 @@ const App = ({ signOut }: props) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${note.name}`}
+                style={{ width: 400 }}
+              />
+            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
